27a6b4b5bf6cc0f3bba249231df26ac983a7b5d5
YOUR NAME <YOUR@EMAIL.com>
2021-03-04 18:40:38 +0700

    init project

---
+++ database/seeders/ProductsSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProductsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('products')->insert([
+        'name'=>'Kẹo dẻo',
+        'categoryId'=>1,
+        'price'=>'5000',
+        'description'=>'123123',
+    ]);
+    }
+}
