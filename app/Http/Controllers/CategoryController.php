52e533ed69f42ecab10a0830a092ddb4550f9231
YOUR NAME <YOUR@EMAIL.com>
2021-03-06 22:50:52 +0700

    create controler

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -9,7 +9,50 @@ class CategoryController extends Controller
 {
 
     function index(){
-    $categories = Category::all();
+        $categories = Category::all();
         dd($categories);
+        //return view('admin.category.index','$categories);
     }
+
+    function add(){
+        //return view('admin.category.add');
+    }
+
+    function postAdd(Request $request){
+        $category = new Category();
+        $category->name = $request->name;
+        $category->description = $request->description;
+        $category->save();
+
+        $categories = Category::all();
+        dd($categories);
+        //return view('admin.category.index','$categories);
+    }
+
+    function update($id){
+        $category = Category::find($id);
+        dd($category);
+        //return view('admin.category.edit','$category');
+    }
+
+    function postUpdate(Request $request){
+        $category = Category::find($request->id);
+        $category->name = $request->name;
+        $category->description = $request->description;
+        $category->save();
+
+        $categories = Category::all();
+        dd($categories);
+        //return view('admin.category.index','$categories);
+    }
+
+    function delete(Request $id){
+        $category = Category::find($id);
+        $category->delete();
+
+        $categories = Category::all();
+        dd($categories);
+        //return view('admin.category.index','$categories);
+    }
+
 }
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,61 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    function index(){
+        $products = Product::all();
+        dd($products);
+        //return view('admin.product.index','$products);
+    }
+
+    function add(){
+        $categories = Product::all();
+        dd($categories);
+        //return view('admin.product.add','$categories);
+    }
+
+    function postAdd(Request $request){
+        $product = new Product();
+        $product->name = $request->name;
+        $product->description = $request->description;
+        $product->price = $request->price;
+        $product->categoryId = $request->categoryId;
+        $product->save();
+
+        $products = Product::all();
+        dd($products);
+        //return view('admin.product.list','$products');
+    }
+
+    function update($id){
+        $product = Product::find($id);
+        dd($product);
+        //return view('admin.product.edit','$product');
+    }
+
+    function postUpdate(Request $request){
+        $product = Product::find($request->id);
+        $product->name = $request->name;
+        $product->description = $request->description;
+        $product->price = $request->price;
+        $product->categoryId = $request->categoryId;
+        $product->save();
+
+        $products = Product::all();
+        dd($products);
+        //return view('admin.product.list','$products');
+    }
+
+    function delete(Request $id){
+        $product = Product::find($id);
+        $product->delete();
+
+        $products = Product::all();
+        dd($products);
+        //return view('admin.product.list','$products');
+    }
 }
